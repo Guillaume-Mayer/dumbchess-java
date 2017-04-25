213c988bf709efacd7032a9486a6de1457cf40ab
Guillaume Mayer <gmayer@arkho.tech>
2017-04-25 19:11:20 -0300

    More random plays

--- dumbchess-test/src/test/java/chess/LegalMovesTest.java
+++ dumbchess-test/src/test/java/chess/LegalMovesTest.java
@@ -31,7 +31,7 @@ public class LegalMovesTest {
 		System.out.println("Random Seed = " + seed);
 		Random random = new Random(seed);
 		int halfMoveCount = 0;
-		while (halfMoveCount < 100 && assertAndPlay(random) > 0) {
+		while (halfMoveCount <= 200 && assertAndPlay(random) > 0) {
 			halfMoveCount ++;
 		}
 		System.out.println("HalfMoveCount: " + halfMoveCount);
